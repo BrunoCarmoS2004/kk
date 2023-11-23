7d03236f17ddb354da014dea3665dee5a3f06de1
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-11-22 22:00:47 -0300

    DSA

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,39 +1,319 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
+import { testUserAgent } from '@ionic/core/dist/types/utils/platform';
+import { timer } from 'rxjs';
 
-import { InfiniteScrollCustomEvent, IonInfiniteScroll } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  data: any[] = []
-  num1: number = 0;
+export class HomePage {
+
+
+  tudo: string = "0"
+  resul: string = ""
+  conta: string = ""
+  raizq: string = ""
+  raizqint: number = 0
+  raizqint2: number = 0
+  reset: boolean = true
+  raizcheck: boolean = false
+  delnot: boolean = false
+  sinalcheck: number = 0
+  resultados: string [] = []
+  contas: string [] = []
+
+
   constructor() {}
 
-  ngOnInit() {
+
+  limparhist(){
+    for(var i = 0; i <= this.resultados.length; i++){
+      this.resultados.pop()
+      this.contas.pop()
+    }
+  }
+
+
+  digitos(valor: string){
+    this.sinalcheck = 0
+    if (this.reset == false){
+      this.limpar()
+    }
+    if (this.tudo == "0"){
+      this.tudo=""
+    }
+    if (this.raizcheck == true){
+      this.raizqint2 = 0
+      this.raizq += valor
+      this.raizqint = parseInt(this.raizq)
+      this.raizqint2 = Math.sqrt(this.raizqint)
+      this.tudo += valor
+    }
+    if (this.raizcheck != true){
+      this.tudo += valor
+      this.conta += valor  
+    }
+  }
+
+  digitosHistorico(valor:string){
+    if(valor == null){
+    }else{
+      this.tudo += valor
+      this.conta += valor  
+    }
+  }
+ 
+  soma(){
+    if (this.sinalcheck == 1){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -1)
+    }
+    if (this.sinalcheck == 2){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -2)
+    }
+    if(this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+      this.reset = true
+      this.tudo = this.tudo.replace('= ','');
+      this.tudo += "+"  
+      this.conta += '+'
+      this.sinalcheck = 1
+      this.delnot = false
+      this.raizcheck = false
+  }
+
+
+
+
+  sub(){
+    if (this.sinalcheck == 1){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -1)
+    }
+    if (this.sinalcheck == 2){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -2)
+    }
+    if(this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+    this.reset = true
+    this.tudo = this.tudo.replace('= ','');
+    this.tudo += "-"
+    this.conta += '-'
+    this.sinalcheck = 1
+    this.delnot = false
+    this.raizcheck = false
+  }
+
+
+
+
+  multi(){
+    if (this.sinalcheck == 1){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -1)
+    }
+    if (this.sinalcheck == 2){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -2)
+    }
+    if(this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+    this.reset = true
+    this.tudo = this.tudo.replace('= ','');
+    this.tudo += "x"
+    this.conta += '*'
+    this.sinalcheck = 1
+    this.delnot = false
+    this.raizcheck = false
+  }
+
+
+
+
+  divi(){
+    if (this.sinalcheck == 1){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -1)
+    }
+    if (this.sinalcheck == 2){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -2)
+    }
+    if(this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+    this.reset = true
+    this.tudo = this.tudo.replace('= ','');
+    this.tudo += "÷"
+    this.conta += '/'
+    this.sinalcheck = 1
+    this.delnot = false
+    this.raizcheck = false
+  }
+
+
+
+
+  pi(){
+    if (this.reset == false){
+      this.limpar()
+    }
+    if (this.tudo == "0"){
+      this.tudo=""
+    }
+    this.tudo += "𝝅"
+    this.conta += '3.1415926535'
+  }
+
+
+
+
+  ponto(){
+    if (this.reset == false){
+      this.limpar()
+    }
+    this.tudo += ","
+    this.conta +='.'
+  }
+
+
+
+
+  paren1(){
+    if (this.reset == false){
+      this.limpar()
+    }
+    if (this.tudo == "0"){
+      this.tudo=""
+    }
+    this.tudo += "("
+    this.conta += '('
+  }
+
+
+
+
+  paren2(){
+    if (this.reset == false){
+      this.limpar()
+    }
+    if (this.tudo == "0"){
+      this.tudo=""
+    }
+    this.tudo += ")"
+    this.conta += ')'
+  }
+
+
+
+
+  porcem(){
+    this.reset = true
+    this.tudo = this.tudo.replace('= ','');
+    this.tudo += "%"
+    this.conta += '/100'
+  }
+
+
+
+
+  elevado(){
+    if (this.sinalcheck == 1){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -1)
+    }
+    if (this.sinalcheck == 2){
+      this.tudo = this.tudo.slice(0, -1)
+      this.conta = this.conta.slice(0, -2)
+    }
+    if(this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+    this.reset = true
+    this.tudo = this.tudo.replace('= ','');
+    this.tudo += "^"
+    this.conta += '**'
+    this.sinalcheck = 2
+    this.delnot = false
+    this.raizcheck = false
+  }
+
+
+
+
+  raiz(){
+    if (this.reset == false){
+      this.limpar()
+    }
+    if (this.tudo == "0"){
+      this.tudo=""
+    }
+    this.tudo += "√"
+    this.raizcheck = true
+  }
+
+
+
+
+  del(){
+    if (this.delnot == false){
+      if (this.tudo != ""){
+        this.tudo = this.tudo.slice(0, -1)
+        this.conta = this.conta.slice(0, -1)
+      }
+      if (this.tudo == ""){
+        this.tudo += 0
+      }
+    }
+  }
+
+
+
+
+  resultado(){
+    if (this.raizcheck == true){
+      this.conta += this.raizqint2
+    }
+    if (this.tudo == "0"){
+      this.conta = "0"
+    }
+    this.conta = eval(this.conta)
+    this.resul = this.tudo
+    this.tudo = this.conta
+    this.tudo = "= "+this.tudo
+    this.contas.unshift(this.tudo)
+    this.resultados.unshift(this.resul)
+    this.reset = false
+    this.delnot = true
+    this.raizcheck = false
     
   }
 
-  /*loadData(event:any){
-    setTimeout(()=>{
-      for(var i = 0;i<10;i++ ){
-        this.data.push(Date.now().toString());
-      }
-      event.target.complete();
-      if(this.data.length == 1000){
-        event.target.disabled = true;
-      }
-    },2000)
 
+
+
+  limpar(){
+    this.tudo = "0"
+    this.resul = ""
+    this.conta = ""
+    this.raizq = ""
+    this.raizqint = 0
+    this.raizqint2 = 0
+    this.reset = true
+    this.delnot = false
+    this.raizcheck = false
   }
-*/
-  teste(){
-      this.num1 +=1
-      this.data.push(this.num1)
-    }
-  }
+}
+
 
 
 
